683d6b7a88c5cd42547bdd48de50e200cf4f9f17
icymars0815 <130199035+icymars0815@users.noreply.github.com>
2023-04-10 08:57:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'QI',
   email: 'info@qiwangumass.com',
   link: 'https://nobelium.vercel.app',
-  description: '欢迎光临，这只是我用来随手记录东西的地方',
+  description: 'Welcome and have a seat',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
